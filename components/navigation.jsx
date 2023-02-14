97e727408279a55f4c22409bf150f29c07daa739
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-02-13 21:41:25 -0800

    nav bar content setup

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -1,29 +1,28 @@
 import React from "react";
+import Image from "next/image";
+import { NavbarBrand } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../public/images/logo.svg";
+
 export default function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+      <Container className="nav-container">
+        <Nav className="nav-left">
+          <Navbar.Brand href="#home">
+            <Image src={logo} />
+            NFT Marketplace
+          </Navbar.Brand>
+        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav justify-content-end">
+          <Nav className="nav-right">
+            <Nav.Link href="#home">Marketplace</Nav.Link>
+            <Nav.Link href="#link">Rankings</Nav.Link>
+            <Nav.Link href="#link">Connect a wallet</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
