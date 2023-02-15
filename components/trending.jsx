bfd7b8f6c9b822ebc47d51dc1f13f57aa35fe1f7
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-02-14 20:03:24 -0800

    trending collection card images skeleton

--- components/trending.jsx
+++ components/trending.jsx
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import trendPlaceholder from "../public/images/Trending-Placeholder.png";
 
 export default function Trending() {
   return (
@@ -12,13 +13,30 @@ export default function Trending() {
       <h3 className="trending-desc">
         Checkout Our Weekly Updated Trending Collection.
       </h3>
-      <Container className="trending-collection">
+      <div className="trending-collection">
         <Row>
-          <Col sm={4}>Test</Col>
+          <Col sm={4}>
+            <Row>
+              <Col sm={12}>
+                <Image src={trendPlaceholder} />
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={4}>img</Col>
+              <Col sm={4}>img</Col>
+              <Col sm={4}>img</Col>
+            </Row>
+            <h3 className="trending-collection-name">DSGN Animals</h3>
+            <div className="trending-collection-info">
+              <Image src={trendPlaceholder} width={50} />
+              <span className="trending-collection-creator">MrFox</span>
+            </div>
+          </Col>
+
           <Col sm={4}>Test</Col>
           <Col sm={4}>Test</Col>
         </Row>
-      </Container>
+      </div>
     </div>
   );
 }
