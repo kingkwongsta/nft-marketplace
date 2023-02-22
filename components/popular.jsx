6994cefad9cf62f62794b9ac8b12e93e4060b07b
kwongster <bryant.j.kwong@gmail.com>
2023-02-21 19:55:54 -0800

    styling for collection divs

--- components/popular.jsx
+++ components/popular.jsx
@@ -4,12 +4,12 @@ import test from "../public/images/Image Placeholder.png";
 
 export default function Popular() {
   function renderPopular() {
-    const popularElements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const popularElements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     return popularElements.map((x, key) => {
       return (
         // eslint-disable-next-line react/jsx-key
-        <div className="max-w-[200px]">
-          <Image className="max-w-[120px]" src={test} alt="test" />
+        <div className="max-w-[300px] text-center ">
+          <Image className="max-w-[200px] block m-auto" src={test} alt="test" />
           <p className="text-l">Collection</p>
           <p>
             Floor Price: <span>100 ETH</span>
@@ -27,7 +27,7 @@ export default function Popular() {
       <h3 className="text-2xl mb-12 font-normal leading-normal">
         Checkout the top Creators on the Ethereum Network
       </h3>
-      <div className="flex">{renderPopular()}</div>
+      <div className="grid grid-cols-3">{renderPopular()}</div>
     </>
   );
 }
