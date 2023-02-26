a82c300dd4a75a05b9e35d73ada6ec42122c5166
kwongster <bryant.j.kwong@gmail.com>
2023-02-26 00:12:05 -0800

    styling for logo

--- components/collection.jsx
+++ components/collection.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import trendPlaceholder from "../public/images/Trending-Placeholder.png";
+import Popular from "./popular";
 import { nftData, salesData } from "../pages/api/nftPortData";
 import data2 from "../pages/api/cleanNFTData";
 
@@ -21,48 +21,58 @@ export default function Collection() {
       metric: "Total Vol:",
       amount: `${Math.round(salesData.statistics.total_volume)} ETH`,
     },
+    {
+      metric: "Market Cap:",
+      amount: `${Math.round(salesData.statistics.market_cap)} ETH`,
+    },
+    {
+      metric: "Avg Sale(24h):",
+      amount: `${
+        Math.round(salesData.statistics.one_day_average_price * 10) / 10
+      } ETH`,
+    },
+    {
+      metric: "Owners:",
+      amount: `${salesData.statistics.num_owners}`,
+    },
+    {
+      metric: "Supply:",
+      amount: `${Math.round(salesData.statistics.total_supply)}`,
+    },
   ];
 
   function renderStats() {
     return stat.map((stat, index) => {
       return (
-        <div className="flex justify-between" key={index}>
+        <div className="flex justify-between bg-zinc-700 p-2" key={index}>
           <div>{stat.metric}</div>
           <div>{stat.amount}</div>
         </div>
       );
     });
   }
-
   return (
-    <div className="container flex flex-row">
-      <div className="section-img basis-1/5">
-        <Image
-          src={nftData.contract.metadata.cached_thumbnail_url}
-          width={200}
-          height={200}
-          alt="placeholder"
-        />
-      </div>
-      <div className="section-stats basis-2/5">
-        <div className="stat-title text-4xl font-medium mb-8">
-          {nftData.contract.name.replace(/([A-Z])/g, " $1").trim()}
+    <>
+      <div className="container flex flex-row">
+        <div className="section-img basis-1/5 flex justify-center">
+          <Image
+            className="min-w-[230px]"
+            src={nftData.contract.metadata.cached_thumbnail_url}
+            width={200}
+            height={200}
+            alt="placeholder"
+          />
         </div>
-        <div className="stat-details-section grid grid-cols-2 gap-x-14 gap-y-4 text-xlg uppercase">
-          {renderStats()}
-          <div className="">
-            Market Cap: {Math.round(salesData.statistics.market_cap)} ETH
+        <div className="section-stats basis-2/5">
+          <div className="stat-title text-4xl font-medium mb-8">
+            {nftData.contract.name.replace(/([A-Z])/g, " $1").trim()}
           </div>
-          <div className="">
-            Avg Sale(24h):{" "}
-            {Math.round(salesData.statistics.one_day_average_price * 10) / 10}{" "}
-            ETH
+          <div className="stat-details-section grid grid-cols-2 gap-x-14 gap-y-4 text-xlg uppercase">
+            {renderStats()}
           </div>
-          <div className="">Owners: {salesData.statistics.num_owners}</div>
-          <div className="">Supply: {salesData.statistics.total_supply}</div>
         </div>
+        <div className="section-info basis-2/5">info</div>
       </div>
-      <div className="section-info basis-2/5">info</div>
-    </div>
+    </>
   );
 }
