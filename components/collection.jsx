1b9156956d466a23ec10b774fd3eae201e540ea5
kwongster <bryant.j.kwong@gmail.com>
2023-02-26 00:00:32 -0800

    configure dynamically generate stats divs through map function of renderstats()

--- components/collection.jsx
+++ components/collection.jsx
@@ -12,6 +12,28 @@ export default function Collection() {
   const index = data2.findIndex((x) => x.collection === collectionName);
   console.log(index);
 
+  const stat = [
+    {
+      metric: "Floor:",
+      amount: `${salesData.statistics.floor_price} ETH`,
+    },
+    {
+      metric: "Total Vol:",
+      amount: `${Math.round(salesData.statistics.total_volume)} ETH`,
+    },
+  ];
+
+  function renderStats() {
+    return stat.map((stat, index) => {
+      return (
+        <div className="flex justify-between" key={index}>
+          <div>{stat.metric}</div>
+          <div>{stat.amount}</div>
+        </div>
+      );
+    });
+  }
+
   return (
     <div className="container flex flex-row">
       <div className="section-img basis-1/5">
@@ -26,16 +48,13 @@ export default function Collection() {
         <div className="stat-title text-4xl font-medium mb-8">
           {nftData.contract.name.replace(/([A-Z])/g, " $1").trim()}
         </div>
-        <div className="stat-details-section grid grid-cols-2 gap-3 text-xl uppercase">
-          <div>Floor: {salesData.statistics.floor_price} ETH</div>
-          <div className="">
-            Total Vol: {Math.round(salesData.statistics.total_volume)} ETH
-          </div>
+        <div className="stat-details-section grid grid-cols-2 gap-x-14 gap-y-4 text-xlg uppercase">
+          {renderStats()}
           <div className="">
             Market Cap: {Math.round(salesData.statistics.market_cap)} ETH
           </div>
           <div className="">
-            Average Sale(24h):{" "}
+            Avg Sale(24h):{" "}
             {Math.round(salesData.statistics.one_day_average_price * 10) / 10}{" "}
             ETH
           </div>
