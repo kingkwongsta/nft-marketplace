98a0d218726ccca483c27e80d194da8839bca0d7
kwongster <bryant.j.kwong@gmail.com>
2023-03-02 21:06:49 -0800

    delayed display using useEffect

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,11 +1,20 @@
 import Hero from "@/components/hero";
 import LazyLoadComponent from "@/components/tools/LazyLoadComponent";
+import Popular from "@/components/popular";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [showPopular, setPopularShow] = useState(true);
+  useState(() => {
+    setTimeout(() => {
+      setPopularShow((x) => !x);
+    }, 1000);
+  });
+
   return (
     <>
       <Hero />
-      <LazyLoadComponent />
+      {showPopular ? <></> : <Popular />}
     </>
   );
 }
