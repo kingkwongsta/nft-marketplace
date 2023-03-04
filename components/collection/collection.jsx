83a93b722eeb8afd23e9776d2369f7ca48e1dc98
kwongster <bryant.j.kwong@gmail.com>
2023-03-03 23:34:15 -0800

    clean up code, comments, refactor collection images to be separate component called gallery

--- components/collection/collection.jsx
+++ components/collection/collection.jsx
@@ -1,34 +1,25 @@
 import { useState, useEffect } from "react";
-import Image from "next/image";
-//HARDCODED DATA FROM API CALLS
-// import { nftData, salesData } from "../pages/api/nftPortData";
 import CollectionInfo from "./collectioninfo";
-import { getSales, getCollection } from "../pages/api/nftport.js";
-import topNFTData from "../pages/api/topNFTData";
+import Gallery from "./gallery";
+//API CALLS
+import { getSales, getCollection } from "../../pages/api/nftport.js";
 
-const address = [
-  { name: "BAYC", address: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D" },
-  { name: "Azuki", address: "0xED5AF388653567Af2F388E6224dC7C4b3241C544" },
-  { name: "Doodles", address: "0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e" },
-  {
-    name: "BoredApeKennelClub",
-    address: "0xba30e5f9bb24caa003e9f2f0497ad287fdf95623",
-  },
-  {
-    name: "MGLand",
-    address: "0xe75512aa3bec8f00434bbd6ad8b0a3fbff100ad6",
-  },
-];
+//!!!!!!Need to replace this with variable containing all NFT address in PudgeyPenguins format!!!!
+import topNFTData from "../../pages/api/topNFTData";
 
 export default function Collection({ nftcollection }) {
+  //NFT Sales/Transaction Data - getSales API Call
   const [salesData, setSalesData] = useState();
+  //NFT Image Data - getCollection API Call
   const [nftData, setNFTData] = useState();
 
+  //Pull data from API calls on page render (once)
   useEffect(() => {
     getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  //Async function to get data from APIs
   const getData = async () => {
     try {
       //FIND WHICH INDEX HAS THE DATA
@@ -37,11 +28,13 @@ export default function Collection({ nftcollection }) {
           return x.name;
         })
         .indexOf(nftcollection);
+      //Get Sales Data
       const res = await getSales(topNFTData[index].contract_address);
+      //Get NFT Data - used time out due to rate limiting of API calls
       setTimeout(async () => {
         const res2 = await getCollection(topNFTData[index].contract_address);
         setNFTData(res2);
-      }, 1000);
+      }, 200);
       setSalesData(res);
       console.log(res);
     } catch (err) {
@@ -52,21 +45,7 @@ export default function Collection({ nftcollection }) {
   function renderCollection() {
     console.log(nftData);
     return nftData.nfts.map((nft, index) => {
-      return (
-        <div className="card" key={index}>
-          {nft.cached_file_url && (
-            <Image
-              className=""
-              src={nft.cached_file_url}
-              width="250"
-              height="250"
-              alt="bayc"
-            />
-          )}
-          {/* <p>{nft.cached_file_url}</p> */}
-          <p className="text-lg my-2 mb-8">#{nft.token_id}</p>
-        </div>
-      );
+      return <Gallery key={index} nft={nft} />;
     });
   }
 
@@ -74,7 +53,7 @@ export default function Collection({ nftcollection }) {
     <div className="mx-36">
       <div>
         {nftData === undefined ? (
-          <p>Loading Due to API Rate Limit</p>
+          <p>Loading</p>
         ) : (
           <CollectionInfo salesData={salesData} nftData={nftData} />
         )}
