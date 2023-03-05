d86a38c0084d7891192d4f1005e1705251abd72f
kwongster <bryant.j.kwong@gmail.com>
2023-03-04 21:48:46 -0800

    secondary menu item/pages are row-reverse starting from right side

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -5,6 +5,7 @@ export default function Navigation() {
   return (
     <nav class="flex items-center justify-between flex-wrap bg-slate-500 p-6">
       <div class="flex items-center flex-shrink-0 text-white mr-6">
+        {/* Logo Below */}
         {/* <svg
           class="fill-current h-8 w-8 mr-2"
           width="54"
@@ -14,7 +15,7 @@ export default function Navigation() {
         >
           <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
         </svg> */}
-        <span class="font-semibold text-xl tracking-tight">NFT Browse</span>
+        <span class="font-semibold text-xl tracking-tight">NFT Browser</span>
       </div>
       <div class="block lg:hidden">
         <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
@@ -28,8 +29,8 @@ export default function Navigation() {
           </svg>
         </button>
       </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
+      <div class="w-full block lg:flex lg:items-center lg:w-auto lg:flex-row-reverse">
+        <div class="text-4xl lg:flex-grow">
           <Link
             href="/popular/"
             class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
