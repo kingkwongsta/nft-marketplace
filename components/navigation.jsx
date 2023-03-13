6105935be82ce6e8964d326df8c30159fd4256dd
kwongster <bryant.j.kwong@gmail.com>
2023-03-12 21:15:24 -0700

    added home link with nft browser nav item

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-slate-500 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-slate-500 p-6 mb-20">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         {/* Logo Below */}
         {/* <svg
@@ -15,9 +15,12 @@ export default function Navigation() {
         >
           <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
         </svg> */}
-        <span className="font-semibold text-xl tracking-tight">
+        <Link
+          href="/"
+          className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 text-3xl"
+        >
           NFT Browser
-        </span>
+        </Link>
       </div>
       <div className="block lg:hidden">
         <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
